3c2ed755bdf72a07410081ff8f1e3f4a7f14615e
dev_muslim <jmappsmuslim@gmail.com>
2019-11-23 10:02:34 +0300

    Added collapsing toolbar with textViews

--- app/src/main/kotlin/jmapps/questions200/MainActivity.kt
+++ app/src/main/kotlin/jmapps/questions200/MainActivity.kt
@@ -4,30 +4,41 @@ import android.annotation.SuppressLint
 import android.content.SharedPreferences
 import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.DialogFragment.STYLE_NORMAL
 import androidx.preference.PreferenceManager
+import androidx.viewpager.widget.ViewPager
 import com.google.android.material.appbar.AppBarLayout
 import jmapps.questions200.data.database.DatabaseAsset
+import jmapps.questions200.data.database.DatabaseLists
 import jmapps.questions200.presentation.mvp.other.OtherContract
 import jmapps.questions200.presentation.mvp.other.OtherPresenterImpl
-import jmapps.questions200.presentation.ui.about.AboutUs
-import jmapps.questions200.presentation.ui.chapters.ChaptersFragment
-import jmapps.questions200.presentation.ui.favorites.FavoritesFragment
+import jmapps.questions200.presentation.ui.about.AboutUsBottomSheet
+import jmapps.questions200.presentation.ui.chapters.ChaptersBottomSheet
+import jmapps.questions200.presentation.ui.chapters.ModelChapters
+import jmapps.questions200.presentation.ui.favorites.FavoritesBottomSheet
 import jmapps.questions200.presentation.ui.main.SectionsPagerAdapter
-import jmapps.questions200.presentation.ui.settings.SettingsFragment
+import jmapps.questions200.presentation.ui.main.StringBuilders
+import jmapps.questions200.presentation.ui.settings.SettingsBottomSheet
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), OtherContract.OtherView, ChaptersFragment.GetChapterItem,
-    FavoritesFragment.GetFavoriteItem, View.OnClickListener {
+class MainActivity : AppCompatActivity(), OtherContract.OtherView, ChaptersBottomSheet.GetChapterItem,
+    FavoritesBottomSheet.GetFavoriteItem, View.OnClickListener, ViewPager.OnPageChangeListener {
+
+    private lateinit var preferences: SharedPreferences
+    private lateinit var editor: SharedPreferences.Editor
 
     private lateinit var database: SQLiteDatabase
     private lateinit var otherPresenter: OtherPresenterImpl
 
-    private lateinit var preferences: SharedPreferences
-    private lateinit var editor: SharedPreferences.Editor
+    private lateinit var stringBuilders: StringBuilders
+
+    private lateinit var chapterList: MutableList<ModelChapters>
 
     @SuppressLint("CommitPrefEdits")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,10 +46,15 @@ class MainActivity : AppCompatActivity(), OtherContract.OtherView, ChaptersFragm
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+        openDatabase()
+
         preferences = PreferenceManager.getDefaultSharedPreferences(this)
         editor = preferences.edit()
 
         otherPresenter = OtherPresenterImpl(this, this)
+        stringBuilders = StringBuilders(this, database)
+
+        chapterList = DatabaseLists(this).getChapterList
 
         val sectionsPagerAdapter = SectionsPagerAdapter(supportFragmentManager)
         mainViewPager.adapter = sectionsPagerAdapter
@@ -56,8 +72,10 @@ class MainActivity : AppCompatActivity(), OtherContract.OtherView, ChaptersFragm
         fabChapters.setOnClickListener(this)
         fabFavorites.setOnClickListener(this)
 
+        mainViewPager.addOnPageChangeListener(this)
+
+        setTextContent(preferences.getInt("key_pager_position", 0))
         loadPosition()
-        openDatabase()
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -95,24 +113,40 @@ class MainActivity : AppCompatActivity(), OtherContract.OtherView, ChaptersFragm
         }
     }
 
+    override fun onPageScrollStateChanged(state: Int) {}
+
+    override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {}
+
+    override fun onPageSelected(position: Int) {
+        setTextContent(position)
+    }
+
     override fun getItemPosition(position: Int) {
         mainViewPager.currentItem = position
     }
 
     override fun showFavoriteList() {
-        FavoritesFragment().show(supportFragmentManager, "favorites")
+        val favoritesBottomSheet = FavoritesBottomSheet()
+        favoritesBottomSheet.setStyle(STYLE_NORMAL, R.style.BottomSheetStyleFull)
+        favoritesBottomSheet.show(supportFragmentManager, "favorites")
     }
 
     override fun showListChapters() {
-        ChaptersFragment().show(supportFragmentManager, "chapters")
+        val chaptersBottomSheet = ChaptersBottomSheet()
+        chaptersBottomSheet.setStyle(STYLE_NORMAL, R.style.BottomSheetStyleFull)
+        chaptersBottomSheet.show(supportFragmentManager, "chapters")
     }
 
     override fun showSettings() {
-        SettingsFragment().show(supportFragmentManager, "settings")
+        val settingsBottomSheet = SettingsBottomSheet()
+        settingsBottomSheet.setStyle(STYLE_NORMAL, R.style.BottomSheetStyleFull)
+        settingsBottomSheet.show(supportFragmentManager, "settings")
     }
 
     override fun showAboutUs() {
-        AboutUs().show(supportFragmentManager, "about")
+        val aboutUsBottomSheet = AboutUsBottomSheet()
+        aboutUsBottomSheet.setStyle(STYLE_NORMAL, R.style.BottomSheetStyleFull)
+        aboutUsBottomSheet.show(supportFragmentManager, "about")
     }
 
     override fun onStop() {
@@ -141,5 +175,12 @@ class MainActivity : AppCompatActivity(), OtherContract.OtherView, ChaptersFragm
 
     private fun closeDatabase() {
         database.close()
+    }
+
+    private fun setTextContent(position: Int) {
+        tvQuestionNumber.text = chapterList[position].strChapterNumber
+        tvQuestionContent.movementMethod = LinkMovementMethod.getInstance()
+        tvQuestionContent.setText(stringBuilders.stringBuilder(
+            chapterList[position].strChapterTitle!!), TextView.BufferType.SPANNABLE )
     }
 }
