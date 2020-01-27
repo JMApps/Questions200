7b4bebeae00294fc262873aff0e570e17c126936
dev_muslim <jmappsmuslim@gmail.com>
2020-01-27 20:26:08 +0300

    New database file and version

--- app/src/main/kotlin/jmapps/questions200/data/database/DatabaseAsset.kt
+++ app/src/main/kotlin/jmapps/questions200/data/database/DatabaseAsset.kt
@@ -3,7 +3,7 @@ package jmapps.questions200.data.database
 import android.content.Context
 import com.readystatesoftware.sqliteasset.SQLiteAssetHelper
 
-private var databaseVersion = 1
+private var databaseVersion = 2
 
 class DatabaseAsset(context: Context?) :
     SQLiteAssetHelper(context, "Questions200DB", null, databaseVersion) {
